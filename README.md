8ae9272ea9cbfb5ddea9b8ddc9662a967fad25de
Hirofumi Tanigami <tanigami@gmail.com>
2019-01-22 16:00:52 +0900

    Start

--- README.md
+++ README.md
@@ -1,1 +1,64 @@
-laravel-process-log
+# Laravel Process Log
+
+## Installation
+
+```sh
+composer require shippinno/laravel-process-log
+```
+
+## Setup
+
+### For Artisan commands logging
+
+Let `Shippinno\ProcessLog\ProcessLog::class` to be included in bootstrappers of your Console Kernel (usually `App\Console\Kernel`).
+
+```php
+class Kernel extends ConsoleKernel
+{
+    // ...
+ 
+    protected function bootstrappers()
+    {
+        return array_merge(
+            $this->bootstrappers,
+            [LifeCycleLogging::class]
+        );
+    }
+}
+```
+
+Run an Artisan command and it will be logged like below.
+
+```
+[2019-01-22 15:20:13] production.INFO: Starting up process. {"command":"php artisan some:command"} []
+[2019-01-22 15:20:21] production.INFO: Shutting down process. {"time":"8.258[s]","memory":"34[mb]"} []
+```
+
+### For HTTP requests logging
+
+Same for your HTTP Kernel (usually `App\Http\Kernel`)..
+
+```php
+class Kernel extends HttpKernel 
+{
+    // ...
+ 
+    protected function bootstrappers()
+    {
+        return array_merge(
+            $this->bootstrappers,
+            [LifeCycleLogging::class]
+        );
+    }
+}
+```
+
+Make an HTTP request and it will be logged like below.
+
+```
+[2019-01-22 15:41:26] production.INFO: Starting up process. {"method":"GET","uri":"/foo/bar?baz"} []
+[2019-01-22 15:41:26] production.INFO: Shutting down process. {"time":"0.386[s]","memory":"2048[kb]"} []
+```
+
+Logging is disabled if the app environment is set to `testing`.
+
---
+++ src/ProcessLog.php
@@ -0,0 +1,100 @@
+<?php
+declare(strict_types=1);
+
+namespace Shippinno\LaravelProcessLog;
+
+use Illuminate\Foundation\Application;
+use Psr\Log\LoggerAwareTrait;
+use Psr\Log\LoggerInterface;
+
+class LifeCycleLogging
+{
+    use LoggerAwareTrait;
+
+    /**
+     * @var Application
+     */
+    private $app;
+
+    /**
+     * @var float|null
+     */
+    private $startedAt;
+
+    /**
+     * @param LoggerInterface $logger
+     */
+    public function __construct(LoggerInterface $logger)
+    {
+        $this->setLogger($logger);
+        $this->startedAt = microtime(true);
+    }
+
+    /**
+     * @param Application $app
+     */
+    public function bootstrap(Application $app)
+    {
+        $this->app = $app;
+        if (!$app->environment('testing')) {
+            register_shutdown_function([$this, 'logShuttingDown']);
+            $this->logStartingUp();
+        }
+    }
+
+    /**
+     * @return void
+     */
+    public function logStartingUp(): void
+    {
+        if ($this->app->runningInConsole()) {
+            $command = sprintf('php %s', implode(' ', (array)array_get($GLOBALS, 'argv')));
+            $params = compact('command');
+        } else {
+            $method = $_SERVER['REQUEST_METHOD'];
+            $uri = $_SERVER['REQUEST_URI'];
+            $params = compact('method', 'uri');
+        }
+        $this->logger->info('Starting up process.', $params);
+    }
+
+    /**
+     * @return void
+     */
+    public function logShuttingDown(): void
+    {
+        $time = $this->processingTime();
+        $memory = $this->peekMemoryUsage();
+
+        $this->logger->info('Shutting down process.', compact('time', 'memory'));
+    }
+
+    /**
+     * @return string
+     */
+    private function processingTime(): string
+    {
+        return sprintf('%0.3f[s]', microtime(true) - $this->startedAt);
+    }
+
+    /**
+     * @param float $time
+     * @return string
+     */
+    protected function formatProcessingTime(float $time): string
+    {
+        return sprintf('%0.3f[s]', $time);
+    }
+
+    /**
+     * @return string
+     */
+    private function peekMemoryUsage(): string
+    {
+        $usage = memory_get_peak_usage(true);
+
+        return $usage < (1024 * 1024 * 10)
+            ? sprintf('%s[kb]', round($usage / 1024, 1))
+            : sprintf('%s[mb]', round($usage / (1024 * 1024), 2));
+    }
+}
